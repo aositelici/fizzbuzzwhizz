baf470b39b4c0d1faed69f37c7d305caf96e8985
liya <liya013@live.com>
2016-02-23 13:26:34 +0800

    rewrite spec in new thought

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,67 +1,49 @@
 'use strict';
 describe('FizzBuzzWhizz', function() {
-  describe('#lenth1(student, first, second, third)', function() {
-    it('当学生报数，数字为1位，数字是第一个数的倍数时，应该显示Fizz',function() {
-      var result = length1(9,3,5,7);
+  describe('#getElement(student, first, second, third)', function() {
+    it('当学生报数，数字包含第一个数时，应该显示Fizz',function() {
+      var result = getElement(35,3,5,7);
+      expect(result).toEqual('Fizz');
+    });
+    it('当学生报数，数字不包含第一个数的前提下，数字是第一个数的倍数时，应该显示Fizz',function() {
+      var result = getElement(9,3,5,7);
       expect(result).toEqual('Fizz');
     });
 
-    it('当学生报数，数字为1位，数字是第二个数的倍数时，应该显示Buzz',function() {
-      var result = length1(5,3,5,7);
+    it('当学生报数，数字不包含第一个数的前提下，数字是第二个数的倍数时，应该显示Buzz',function() {
+      var result = getElement(5,3,5,7);
       expect(result).toEqual('Buzz');
     });
 
-    it('当学生报数，数字为1位，数字是第一个数的倍数时，应该显示Whizz',function() {
-      var result = length1(7,3,5,7);
+    it('当学生报数，数字不包含第一个数的前提下，数字是第三个数的倍数时，应该显示Whizz',function() {
+      var result = getElement(7,3,5,7);
       expect(result).toEqual('Whizz');
     });
 
-    it('当学生报数，数字为1位，数字不是三个数的倍数时，显示数字本身',function() {
-      var result = length1(1,3,5,7);
-      expect(result).toEqual(1);
-    });
-  });
-
-  describe('#lenth2(student, first, second, third)', function() {
-    it('当学生报数，数字为2位，数字包含第一个数时，应该显示Fizz',function() {
-      var result = length2(35,3,5,7);
-      expect(result).toEqual('Fizz');
-    });
-
-    it('当学生报数，数字为2位，数字是第一个数的倍数时，应该显示Fizz',function() {
-      var result = length2(12,3,5,7);
-      expect(result).toEqual('Fizz');
-    });
-
-    it('当学生报数，数字为2位，数字不包含第一个数的前提下，数字是第二个数的倍数时，应该显示Buzz',function() {
-      var result = length2(20,3,5,7);
-      expect(result).toEqual('Buzz');
-    });
-
-    it('当学生报数，数字为2位，数字不包含第一个数的前提下，数字只是第三个数的倍数时，应该显示Whizz',function() {
-      var result = length2(49,3,5,7);
-      expect(result).toEqual('Whizz');
-    });
-
-    it('当学生报数，数字为2位，数字不包含第一个数的前提下，数字是第一个和第二个数的倍数时，应该显示FizzBuzz',function() {
-      var result = length2(15,3,5,7);
+    it('当学生报数，数字不包含第一个数的前提下，数字是第一个和第二个数的倍数时，应该显示FizzBuzz',function() {
+      var result = getElement(15,3,5,7);
       expect(result).toEqual('FizzBuzz');
     });
 
-    it('当学生报数，数字为2位，数字不包含第一个数的前提下，数字是第一个和第三个数的倍数时，应该显示FizzWhizz',function() {
-      var result = length2(21,3,5,7);
+    it('当学生报数，数字不包含第一个数的前提下，数字是第一个和第三个数的倍数时，应该显示FizzWhizz',function() {
+      var result = getElement(21,3,5,7);
       expect(result).toEqual('FizzWhizz');
     });
 
-    it('当学生报数，数字为2位，数字不包含第一个数的前提下，数字是第二个和第三个数的倍数时，应该显示BuzzWhizz',function() {
-      var result = length2(70,3,5,7);
+    it('当学生报数,数字不包含第一个数的前提下，数字是第二个和第三个数的倍数时，应该显示BuzzWhizz',function() {
+      var result = getElement(70,3,5,7);
       expect(result).toEqual('BuzzWhizz');
     });
 
-    it('当学生报数，数字为2位，数字不是三个数的倍数时，显示数字本身',function() {
-      var result = length2(11,3,5,7);
+    it('当学生报数,数字不包含第一个数的前提下，数字是第一个，第二个和第三个数的倍数时，应该显示FizzBuzzWhizz',function() {
+      var result = getElement(105,3,5,7);
+      expect(result).toEqual('FizzBuzzWhizz');
+    });
+
+    it('当学生报数，数字不是三个数的倍数时，显示数字本身',function() {
+      var result = getElement(11,3,5,7);
       expect(result).toEqual(11);
     });
-  })
 
+  });
 })
