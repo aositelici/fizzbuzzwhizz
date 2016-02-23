941ea95a7567ef66c1f4aed58269081f13a32fc7
liya <liya013@live.com>
2016-02-23 13:26:01 +0800

    rewrite program in different thought

--- src/main.js
+++ src/main.js
@@ -3,67 +3,41 @@
 function FizzBuzzWhizz (first, second, third) {
   var result = [];
   for(var student = 1; student <= 100; student++) {
-    var length = student.toString().length;
-    var element = judgeLength (length, student, first, second, third);
+    var element = getElement (student, first, second, third);
     result.push(element);
   }
   return result;
 }
 
-function judgeLength (length, student, first, second, third) {
+function getElement(student, first, second, third) {
+  var number = student.toString().split("");
+  var result= [];
 
-  var result;
-  if(length === 1) {
-    result = length1(student, first, second, third);
-  }
-  else if(length ===2) {
-    result = length2(student, first, second, third);
-  }
-  else {
-    result = length3(student, first, second, third);
-  }
-  return result;
+   number.forEach(function(element){
+     if(parseInt(element) ===first) {
+       result="Fizz";
+     }
+   })
+   if(result!=="Fizz") {
+     result=[];
+      if (student%first === 0 || student%second === 0 || student%third === 0){
+       if (student%first === 0) {
+         result.push("Fizz");
+       }
+       if (student%second === 0) {
+         result.push("Buzz");
+       }
+       if (student%third === 0) {
+          result.push("Whizz");
+       }
+       return result.join('');
+     }
+     else {
+         return student;
+     }
+   }
+   else {
+     return result;
+   }
 }
-
-function length1(student, first, second, third) {
-    if(student%first === 0) {
-      return "Fizz";
-    }
-    else if (student%second === 0) {
-      return "Buzz";
-    }
-    else if (student%third === 0) {
-      return "Whizz";
-    }
-    else {
-      return student;
-    }
-}
-
-function length2(student, first, second, third) {
-   var result= [];
-    if(student%10 === first || parseInt(student/10) === first) {
-       return "Fizz";
-    }
-    else if (student%first === 0 || student%second === 0 || student%third === 0){
-      if (student%first === 0) {
-        result.push("Fizz");
-      }
-      if (student%second === 0) {
-        result.push("Buzz");
-      }
-      if (student%third === 0) {
-         result.push("Whizz");
-      }
-    }
-    else {
-        return student;
-    }
-    return result.join("");
-}
-
-function length3(student, first, second, third) {
-   return "Fizz";
-}
-
 FizzBuzzWhizz (3,5,7);
